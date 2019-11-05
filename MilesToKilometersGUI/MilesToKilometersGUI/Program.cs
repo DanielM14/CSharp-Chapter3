4949c435790d499662924cb8e5665da32c706bfa
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-11-05 15:24:41 -0600

    update

---
+++ MilesToKilometersGUI/MilesToKilometersGUI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilesToKilometersGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double userInput = Convert.ToInt32(Console.ReadLine());
+            double kilo;
+
+            kilo = userInput * 1.6;
+
+            Console.WriteLine("The amount of kilometers " + kilo);
+            
+
+
+
+
+
+        }
+    }
+}
---
+++ ProjectedRaises/ProjectedRaises/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectedRaises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nextYearSalary;
+            double theNewSalary;
+
+            Console.WriteLine("What is your yearly salary??");
+            int userInputSalary = Convert.ToInt32(Console.ReadLine());
+
+            nextYearSalary = userInputSalary * 0.04;
+
+            Console.WriteLine("Your salary next year is " + nextYearSalary);
+
+            theNewSalary = userInputSalary + nextYearSalary;
+
+            Console.WriteLine("Your new salary for the new year is " + theNewSalary);
+
+        }
+    }
+}
